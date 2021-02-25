f4d3933504e625ec18ace6a98509b4de3150e5fc
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-02-25 02:17:55 +0100

    Add project files.

---
+++ JobCandidate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have?");
+
+            var years = int.Parse(Console.ReadLine());
+
+            switch(years)
+            {
+                case 0:
+                    Console.WriteLine("Inexperienced");
+                    break;
+                case 1:
+                    Console.WriteLine("Junior");
+                    break;
+                case 3:
+                    Console.WriteLine("Intermidate");
+                    break;
+                case 4:
+                    Console.WriteLine("Advanced");
+                    break;
+                default:
+                    Console.WriteLine("Senior");
+                    break;
+            }
+
+        }
+    }
+}
